75c54cee98b09db5496eda493943131c810121ea
Aleksandr Nossov <anosso@taltech.ee>
2025-02-18 18:50:01 +0200

    RepositoryHelper.cs change

--- tic-tac-two/DAL/RepositoryHelper.cs
+++ tic-tac-two/DAL/RepositoryHelper.cs
@@ -2,11 +2,11 @@
 
 public static class RepositoryHelper
 {
-    // private static readonly AppDbContextFactory Factory = new AppDbContextFactory();
-    // private static readonly AppDbContext DbContext = Factory.CreateDbContext(Array.Empty<string>());
-    // public static readonly IGameRepository GameRepository = new GameRepositoryDb(DbContext);
-    // public static readonly IConfigRepository ConfigRepository = new ConfigRepositoryDb(DbContext);
+    private static readonly AppDbContextFactory Factory = new AppDbContextFactory();
+    private static readonly AppDbContext DbContext = Factory.CreateDbContext(Array.Empty<string>());
+    public static readonly IGameRepository GameRepository = new GameRepositoryDb(DbContext);
+    public static readonly IConfigRepository ConfigRepository = new ConfigRepositoryDb(DbContext);
     
-    public static readonly IGameRepository GameRepository = new GameRepositoryJson();
-    public static readonly IConfigRepository ConfigRepository = new ConfigRepositoryJson();
+    // public static readonly IGameRepository GameRepository = new GameRepositoryJson();
+    // public static readonly IConfigRepository ConfigRepository = new ConfigRepositoryJson();
 }
